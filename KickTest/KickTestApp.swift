8ad4a8a3433b1e97010c2526d671735d4b4a3bb1
Kevin Cohen <kevin.cohen8716@gmail.com>
2025-11-20 15:20:07 -0500

    Initial Commit

---
+++ KickTest/KickTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickTestApp.swift
+//  KickTest
+//
+//  Created by Kevin Cohen on 2025-11-20.
+//
+
+import SwiftUI
+
+@main
+struct KickTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
